d34ab35f1a3318d8dffac55f8855d4b7b105124f
pearsozj <pearsozj@dukes.jmu.edu>
2021-10-13 19:07:53 -0700

    psi2kpa

--- converters.py
+++ converters.py
@@ -4,3 +4,15 @@
 # YOUR CODE GOES IN THIS FILE
 # Update the tests file to uncomment and run tests as you go
 #
+# def psi2kpa ():
+#     return (psi * 6.89476)
+
+psi = eval(input()) 
+psi_conversion_rate = 6.89476
+
+psi2kpa = (psi * psi_conversion_rate)
+
+print(psi2kpa)
+
+
+
